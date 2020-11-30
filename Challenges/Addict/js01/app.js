241e64b0990ba617142bb138c1a7286ba53de098
‘Juliano <julianoperins@gmail.com>
2020-11-29 21:31:07 -0500

    Added new folders

--- Challenges/Addict/js01/app.js
+++ Challenges/Addict/js01/app.js
@@ -1,11 +1,13 @@
 function firstWord(str) {
   const words = str.split(" ").map((word) => {
+
     //let firstLetter = word.slice(0, 1);
     //let rest = word.slice(1);
     //firstLetter = firstLetter.toUpperCase();
     //console.log(firstLetter, rest);
     //return `${firstLetter}${rest}`;
     //OR
+    
     return word.charAt(0).toUpperCase() + word.slice(1);
   });
 
