3248164e5d5e9a5891fa08def0a90d78ee585956
‘Juliano <julianoperins@gmail.com>
2020-12-02 15:41:34 -0500

    Finished the fourth challenge

--- Jona/part-1/js01/app.js
+++ Jona/part-1/js01/app.js
@@ -60,7 +60,8 @@ const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 */
 
-const input = prompt();
+// const input = prompt();
+const input = 500;
 console.log(`The price of the bill is: ${input}`);
 
 const bill = parseInt(input);
@@ -70,3 +71,87 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total price was $
   bill + tip
 }
 `);
+
+console.log("===============================");
+
+function calcAge1(birthYear) {
+  return 2020 - birthYear;
+}
+
+const myDOB = calcAge1(1991);
+console.log(myDOB);
+
+const calcAge2 = function (birthYear) {
+  return 2020 - birthYear;
+};
+
+const age2 = calcAge2(1992);
+console.log(age2);
+
+// Functions in deep
+
+console.log("===============================");
+
+const calcAge = function (year) {
+  return 2020 - year;
+};
+
+const yearsUntilRetirement = function (DOB, firstName) {
+  const age = calcAge(DOB);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    return retirement;
+  }
+
+  if (retirement < 0) {
+    return `He/she is already retired!`;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, "Juliano"));
+console.log(yearsUntilRetirement(1940, "Juliano"));
+
+console.log("===============================");
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// Test Data
+const DolphinsAverage = calcAverage(500, 23, 71);
+const KoalasAverage = calcAverage(65, 54, 49);
+console.log(DolphinsAverage);
+console.log(KoalasAverage);
+
+function checkWinner(DolphinsAverage, KoalasAverage) {
+  if (DolphinsAverage >= 2 * KoalasAverage) {
+    console.log(`Koalas win (${KoalasAverage} vs ${DolphinsAverage})`);
+  } else if (KoalasAverage >= 2 * DolphinsAverage) {
+    console.log(`Dolphins win (${DolphinsAverage} vs ${KoalasAverage})`);
+  } else if (KoalasAverage === DolphinsAverage) {
+    console.log("There is a draw!");
+  } else {
+    console.log("Nobody won the game!");
+  }
+}
+
+checkWinner(DolphinsAverage, KoalasAverage);
